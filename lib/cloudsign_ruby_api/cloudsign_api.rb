b5fbc4276c68b8730a280ac3e12a737cfe0271ad
Naoya Fujita <wfknaoya0731@gmail.com>
2019-07-13 15:05:09 +0900

    送信するところまで

--- lib/cloudsign_ruby_api/cloudsign_api.rb
+++ lib/cloudsign_ruby_api/cloudsign_api.rb
@@ -33,11 +33,63 @@ class CloudsignApi
     end    
   end
 
+  def make_contract(template_id, name, email, data = [])
+    document = create_documents(template_id)
+    update_receiver_participant(document, name, email)
+
+    sender_widgets = document['files'][0]['widgets'].select { |widget| widget['widget_type'] == 1 }
+    sender_widgets.each_with_index do |widget, i|
+      update_document_detail(document, widget, data[i])
+    end
+    
+    send_document(document)
+  end
+
   def documents
     fetch_access_token
     url = build_url('/documents')
     params = {}
     response = @client.get(url, query: params, header: build_header)
+    JSON.parse(response.body)
+  end
+
+  def create_documents(template_id)
+    fetch_access_token
+    url = build_url('/documents')
+    params = {template_id: template_id}
+    response = @client.post(url, query: params, header: build_header)
+    JSON.parse(response.body)
+  end
+
+  def update_receiver_participant(document, name, email)
+    document_id = document['id']
+    receiver_id = document['participants'][1]['id']    
+    fetch_access_token
+    url = build_url("/documents/#{document_id}/participants/#{receiver_id}")
+    params = {email: email, name: name}
+    response = @client.put(url, query: params, header: build_header)
+    JSON.parse(response.body)
+  end
+
+  def update_document_detail(document, widget, text)
+    document_id = document['id']
+    file_id = widget['file_id']
+    widget_id = widget['id']
+    page = widget['page']
+    participant_id = widget['participant_id']
+    fetch_access_token
+    url = build_url("/documents/#{document_id}/files/#{file_id}/widgets/#{widget_id}")
+    params = {participant_id: participant_id, page: page, text: text}
+    response = @client.put(url, query: params, header: build_header)
+    JSON.parse(response.body)
+  end
+
+  def send_document(document)
+    document_id = document['id']
+    fetch_access_token
+    url = build_url("/documents/#{document_id}")
+    params = {}
+    response = @client.post(url, query: params, header: build_header)
     JSON.parse(response.body)
   end
 
